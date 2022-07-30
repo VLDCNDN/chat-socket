d06d6d6514c26dc3e4317bd9310a93c266d199f1
vldcndn <vldcndn@gmail.com>
2022-07-31 07:40:52 +0800

    update view in client

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import io from 'socket.io-client'
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import io from "socket.io-client";
 
 function App() {
   const [socket, setSocket] = useState(null);
@@ -12,24 +10,38 @@ function App() {
     return () => newSocket.close();
   }, [setSocket]);
 
+  function usersList() {
+    return ["Noel", "Marvin", "Sandra", "Giny"].map((user) => 
+      <li class="text-white hover:bg-neutral-800 first:pt-0 last:pb-0">
+        <a href="/" class="flex justify-between items-center w-full p-4">
+          <p>{user}</p>
+          <div class="h-4 w-4 my-1 border-2 border-white rounded-full bg-green-400"></div>
+        </a>
+      </li>
+    );
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="flex bg-neutral-900 h-screen">
+        <div class="h-full border-r border-neutral-800 w-24"></div>
+        <div class="h-full border-r border-neutral-800 lg:w-96 pt-8 sm:w-2/6">
+          <p class="w-full p-4 font-medium text-4xl text-white">Messages</p>
+          <p class="w-full p-4 font-medium text-neutral-600 uppercase antialiased">
+            All Messages
+          </p>
+          <div class="w-full">
+            <ul class="divide-y divide-neutral-800">
+              { usersList() }
+            </ul>
+          </div>
+        </div>
+        <div class="grow h-full"></div>
+        <div class="shrink h-full border-l border-neutral-800"></div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
