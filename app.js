c685beaf288ad8794b2a0576d4f53e16265e2c70
vldcndn <vldcndn@gmail.com>
2022-07-30 23:00:18 +0800

    add library in client

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var http = require('http');
 const { Server } = require('socket.io');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -18,7 +17,6 @@ const io = new Server(server);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -27,7 +25,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 io.on('connection', socket => {
   console.log('a user connected');
@@ -49,7 +46,6 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
 });
 
 module.exports = {app, server};
